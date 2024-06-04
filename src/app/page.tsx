f1f5ced8c6dca9fd6f28f960f2a7d280fa431a05
muchcreative <robby.j.lem@hotmail.com>
2024-06-03 20:05:57 -0400

    Initial npx app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// SVG circle has width="943" height="851", can set in styles
+// import Circle from '/public/circle_svg.svg';
+
+export default function Home() {
+  return (
+    <main>
+    </main>
+  );
+}
---
+++ src/styles/fonts.ts
@@ -0,0 +1,6 @@
+import { Mukta_Mahee } from 'next/font/google';
+
+export const muktaMahee = Mukta_Mahee({ 
+    weight: ['200', '300', '400', '500', '600', '700', '800'],
+    subsets: ['latin']
+});
